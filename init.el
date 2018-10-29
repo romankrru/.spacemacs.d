43a164925447d5bd513efd54d0fa33121bfe26ba
romankrru <rm07ru@gmail.com>
2018-10-30 00:00:20 +0400

    Enable blink-cursor-mode by default

--- init.el
+++ init.el
@@ -342,6 +342,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  (blink-cursor-mode 1)
   (use-package all-the-icons)
   (setq neo-banner-message nil) ; Disable neotree banner message
   (diff-hl-flydiff-mode '(:global t))
