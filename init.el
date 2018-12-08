47c2bb717598e0985de4943d8de453b4c1972b80
romankrru <rm07ru@gmail.com>
2018-12-08 16:34:57 +0300

    Set "jj" for escape sequence

--- init.el
+++ init.el
@@ -341,6 +341,9 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  
+  (setq-default evil-escape-key-sequence "jj")
+  (setq-default evil-escape-delay 0.2)
 
   (blink-cursor-mode 1)
   (use-package all-the-icons)
